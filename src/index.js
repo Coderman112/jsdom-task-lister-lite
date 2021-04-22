eab8e13d2ed5d9725be085b8ab4b42080627180e
ChristianCudby <christiancudby@hotmail.com>
2021-04-21 20:23:21 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const task = document.getElementById("new-task-description");
+  const submit = document.getElementById("submit");
+  const list = document.getElementById("list");
+  // const taskList = document.getElementById("tasks")
+
+  submit.addEventListener("click", function(event) {
+    event.preventDefault();
+    let todo = task.value;
+    list.innerHTML += `<li>${todo}
+    <button data-action = "delete"> X </button>
+    </li>`;
+  });
+
+  list.addEventListener("click", function (e) {
+    if (e.target.dataset.action === "delete") {
+      e.target.parentElement.remove();
+    }
+  })
+
 });
